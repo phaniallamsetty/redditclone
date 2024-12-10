b85db84a395fd9bcce8b5e43bbec7bbb8a26f030
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2024-12-10 14:23:23 -0500

    Created the CommentRepository

--- src/main/java/com/pallamsetty/redditclone/repository/CommentRepository.java
+++ src/main/java/com/pallamsetty/redditclone/repository/CommentRepository.java
@@ -1,14 +1,15 @@
 package com.pallamsetty.redditclone.repository;
 
+import com.pallamsetty.redditclone.model.Comment;
 import com.pallamsetty.redditclone.model.Post;
 import com.pallamsetty.redditclone.model.User;
-import com.pallamsetty.redditclone.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+    List<Comment> findAllByUser(User user);
 }
